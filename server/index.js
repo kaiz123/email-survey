03087be093aa858501f2bee0fe57b5059dc3c02b
Kaiz <kaiz.growfitter@gmail.com>
2018-10-12 16:49:12 +0530

    second

--- server/index.js
+++ server/index.js
@@ -5,17 +5,25 @@ const keys= require('./config/keys');
 
 const app=express();
 passport.use(new GoogleStrategy({
-	clientID : keys.googleClientId,
+	clientID : keys.googleClientID,
 	clientSecret: keys.googleClientSecret,
 	callbackURL: '/auth/google/callback'
-}, (accessToken)=>{
+}, (accessToken,refreshToken,profile,done)=>{
 
 	console.log(accessToken);
 })
 );
 
+
+app.get('/auth/google', passport.authenticate('google',{
+	scope: ['profile','email']
+}));
+
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 app.get('/', function (req, res) {
   res.send('hello world')
-})
+});
 
 app.listen(5000);
