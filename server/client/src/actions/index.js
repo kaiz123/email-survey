b28ea6fbe4a74aef712718bd28694d570d7dfbcc
Kaiz <kaiz.growfitter@gmail.com>
2018-10-31 13:52:45 +0530

    surverys

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -13,3 +13,18 @@ export const handleToken = token => async dispatch => {
 
   dispatch({ type: FETCH_USER, payload: res.data });
 };
+
+
+
+export const submitSurvey = (values, history) => async dispatch => {
+  const res = await axios.post('/api/surveys', values);
+
+  history.push('/surveys');
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const fetchSurveys = () => async dispatch => {
+  const res = await axios.get('/api/surveys');
+
+  dispatch({ type: FETCH_SURVEYS, payload: res.data });
+};
--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -22,37 +22,37 @@ module.exports = app => {
     res.send('Thanks for voting!');
   });
 
-  app.post('/api/surveys/webhooks', (req, res) => {
-    const p = new Path('/api/surveys/:surveyId/:choice');
+  // app.post('/api/surveys/webhooks', (req, res) => {
+  //   const p = new Path('/api/surveys/:surveyId/:choice');
 
-    _.chain(req.body)
-      .map(({ email, url }) => {
-        const match = p.test(new URL(url).pathname);
-        if (match) {
-          return { email, surveyId: match.surveyId, choice: match.choice };
-        }
-      })
-      .compact()
-      .uniqBy('email', 'surveyId')
-      .each(({ surveyId, email, choice }) => {
-        Survey.updateOne(
-          {
-            _id: surveyId,
-            recipients: {
-              $elemMatch: { email: email, responded: false }
-            }
-          },
-          {
-            $inc: { [choice]: 1 },
-            $set: { 'recipients.$.responded': true },
-            lastResponded: new Date()
-          }
-        ).exec();
-      })
-      .value();
+  //   _.chain(req.body)
+  //     .map(({ email, url }) => {
+  //       const match = p.test(new URL(url).pathname);
+  //       if (match) {
+  //         return { email, surveyId: match.surveyId, choice: match.choice };
+  //       }
+  //     })
+  //     .compact()
+  //     .uniqBy('email', 'surveyId')
+  //     .each(({ surveyId, email, choice }) => {
+  //       Survey.updateOne(
+  //         {
+  //           _id: surveyId,
+  //           recipients: {
+  //             $elemMatch: { email: email, responded: false }
+  //           }
+  //         },
+  //         {
+  //           $inc: { [choice]: 1 },
+  //           $set: { 'recipients.$.responded': true },
+  //           lastResponded: new Date()
+  //         }
+  //       ).exec();
+  //     })
+  //     .value();
 
-    res.send({});
-  });
+  //   res.send({});
+  // });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
